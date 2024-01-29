8046b2cf1cf93cb6d50fa0a76806f19dfe1f2694
Tony MEMPONTEIL <tony.memponteil@gmail.com>
2024-01-29 15:53:59 +0100

    org-control components created to ensure org is on the good route and install shadcn/ui accordion & skeleton + usehooks-ts install

---
+++ app/(platform)/(dashboard)/organization/[organizationId]/_components/org-control.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { useOrganizationList } from "@clerk/nextjs";
+import { useParams } from "next/navigation";
+import { useEffect } from "react";
+
+// Znsures that the URL displayed corresponds to the organization
+
+export const OrgControl = () => {
+  const params = useParams();
+  const { setActive } = useOrganizationList();
+
+  useEffect(() => {
+    if (!setActive) return;
+
+    setActive({
+      organization: params.organizationId as string,
+    });
+  }, [setActive, params.organizationId]);
+
+  return null;
+};
