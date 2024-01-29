70c778b0ed3c1d19cc1d696585ee79554c283afe
Tony MEMPONTEIL <tony.memponteil@gmail.com>
2024-01-29 14:38:39 +0100

    organization page and navbar dashboard created

--- middleware.ts
+++ middleware.ts
@@ -6,7 +6,6 @@ import { authMiddleware } from "@clerk/nextjs";
 export default authMiddleware({
   publicRoutes: ["/"],
 });
-
 export const config = {
   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
