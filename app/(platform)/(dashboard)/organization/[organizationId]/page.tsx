16e0b1fd23569a9b5273d0f9c370b04b679ecf08
Tony MEMPONTEIL <tony.memponteil@gmail.com>
2024-01-30 14:08:29 +0100

    install, setup client prisma

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,7 +1,7 @@
 const OrganizationIdPage = async () => {
   return (
     <div className="w-full mb-20">
-      <div className="px-2 md:px-4"></div>
+      <div className="px-2 md:px-4"> Organization Page</div>
     </div>
   );
 };
