29e3551db62d0cc1c4216c2a2f090a817c3a4956
Tony MEMPONTEIL <tony.memponteil@gmail.com>
2024-02-01 14:42:45 +0100

    hint component created

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,5 +1,7 @@
 // import { Suspense } from "react";
 
+import { Separator } from "@/components/ui/separator";
+import BoardList from "./_components/board-list";
 import { Info } from "./_components/info";
 
 // import { Separator } from "@/components/ui/separator";
@@ -14,6 +16,10 @@ const OrganizationIdPage = async () => {
   return (
     <div className="w-full mb-20">
       <Info />
+      <Separator className="my-4" />
+      <div className="px-2 md:px-4">
+        <BoardList />
+      </div>
     </div>
   );
 };
