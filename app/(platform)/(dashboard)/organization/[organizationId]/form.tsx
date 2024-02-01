ad991d81bbad9b94f4162ad1e37ef1a9c633188b
Tony MEMPONTEIL <tony.memponteil@gmail.com>
2024-02-01 11:39:48 +0100

    formInput reusable created

--- app/(platform)/(dashboard)/organization/[organizationId]/form.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/form.tsx
@@ -1,22 +1,19 @@
 "use client";
 
-import { createBoard } from "@/actions/create-board";
-import { useAction } from "@/hooks/use-action";
-
 export const Form = () => {
-  const { execute, fieldErrors } = useAction(createBoard, {
-    onSuccess(data) {
-      console.log(data, "SUCCESS");
-    },
-    onError(error) {
-      console.log(error);
-    },
-  });
+  // const { execute, fieldErrors } = useAction(createBoard, {
+  //   onSuccess(data) {
+  //     console.log(data, "SUCCESS");
+  //   },
+  //   onError(error) {
+  //     console.log(error);
+  //   },
+  // });
 
-  const onSubmit = (formData: FormData) => {
-    const title = formData.get("title") as string;
-    execute({ title });
-  };
+  // const onSubmit = (formData: FormData) => {
+  //   const title = formData.get("title") as string;
+  //   execute({ title });
+  // };
   return (
     <div>
       {/* <form action={onSubmit}>
