d759abd8e9c271b93067892e1ff394914d6b3b0d
Tony MEMPONTEIL <tony.memponteil@gmail.com>
2024-02-02 17:04:22 +0100

    title dynamicaly metadata added on board page

--- app/(platform)/(dashboard)/board/[boardId]/layout.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/layout.tsx
@@ -3,7 +3,30 @@ import { notFound, redirect } from "next/navigation";
 
 import db from "@/lib/db";
 
-// import { BoardNavbar } from "./_components/board-navbar";
+export async function generateMetadata({
+  params,
+}: {
+  params: { boardId: string };
+}) {
+  const { orgId } = auth();
+
+  if (!orgId) {
+    return {
+      title: "Board",
+    };
+  }
+
+  const board = await db.board.findUnique({
+    where: {
+      id: params.boardId,
+      orgId,
+    },
+  });
+
+  return {
+    title: board?.title || "Board",
+  };
+}
 
 const BoardIdLayout = async ({
   children,
@@ -34,7 +57,6 @@ const BoardIdLayout = async ({
       className="relative h-full bg-no-repeat bg-cover bg-center"
       style={{ backgroundImage: `url(${board.imageFullUrl})` }}
     >
-      {/* <BoardNavbar data={board} /> */}
       <div className="absolute inset-0 bg-black/10" />
       <main className="relative pt-28 h-full">{children}</main>
     </div>
