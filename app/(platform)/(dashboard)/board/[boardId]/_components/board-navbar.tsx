3546b1c602e1fc91e6ff4e8bcbb4455926d3761a
Tony MEMPONTEIL <tony.memponteil@gmail.com>
2024-02-05 15:38:26 +0100

    set up title board option with fetch (detele board)

--- app/(platform)/(dashboard)/board/[boardId]/_components/board-navbar.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/board-navbar.tsx
@@ -1,4 +1,5 @@
 import { Board } from "@prisma/client";
+import BoardOptions from "./board-options";
 import { BoardTitleForm } from "./board-title-form";
 
 interface BoardNavbarProps {
@@ -9,6 +10,9 @@ export const BoardNavbar = async ({ data }: BoardNavbarProps) => {
   return (
     <div className="w-full h-14 z-[40] bg-black/50 fixed top-14 flex items-center px-6 gap-x-4 text-white">
       <BoardTitleForm data={data} />
+      <div className="ml-auto">
+        <BoardOptions id={data.id} />
+      </div>
     </div>
   );
 };
