b47a5ee4bc21617d8d3c77d81bf9427e28db26e0
Tony MEMPONTEIL <tony.memponteil@gmail.com>
2024-02-06 14:21:09 +0100

    static ListHeader without input set up

--- app/(platform)/(dashboard)/board/[boardId]/_components/list-item.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-item.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { ListWithCards } from "@/types";
+import ListHeader from "./list-header";
 
 type ListItemProps = {
   data: ListWithCards;
@@ -10,8 +11,8 @@ type ListItemProps = {
 const ListItem = ({ data, index }: ListItemProps) => {
   return (
     <li className="shrink-0 h-full w-[272px] select-none">
-      <div className="w-full rounded-md bg-[#1f1f2f4] shadow-md pb-2">
-        <ListHeader />
+      <div className="w-full rounded-md bg-[#f1f2f4] shadow-md pb-2">
+        <ListHeader data={data} />
       </div>
     </li>
   );
